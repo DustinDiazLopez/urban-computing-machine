b5a87d8bb42fb1c5a4b71eb5d721c2241c5814fb
Dustin Diaz <hunterghost526@gmail.com>
2022-07-21 04:46:17 -0500

    add plugins and update script

---
+++ Dockerfile
@@ -0,0 +1,3 @@
+FROM nimmis/spigot:latest
+
+
--- mc-server-setup.sh
+++ mc-server-setup.sh
@@ -4,6 +4,7 @@
 sudo apt-get update
 sudo apt-get install screen git openjdk-17-jre-headless htop -y
 
+sudo ufw allow 80
 sudo ufw allow 25565
 
 mkdir -p minecraft
@@ -13,9 +14,11 @@ git config --global --unset core.autocrlf
 java -jar BuildTools.jar --rev latest
 echo "eula=true" > eula.txt
 
+cp ../urban-computing-machine/plugins/. ./plugins/.
+
 # create a detached screen and run the command on the prev created screen
 screen -dmS minecraft-server-spigot
-screen -S minecraft-server-spigot -X stuff 'java -Xms6G -Xmx6G -XX:+UseG1GC -jar spigot-1.17.1.jar nogui\n'
+screen -S minecraft-server-spigot -X stuff 'java -Xms3G -Xmx3G -XX:+UseG1GC -jar spigot-1.9.jar -p 80 -nogui\n'
 
 # screen -ls
 # screen -r <pid>
